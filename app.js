dcb7fbe04e61e84f81e8ef85fa825be571734992
Garrett Adams <garrettadams27@gmail.com>
2022-07-21 16:25:20 -0600

    d

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const hero = {
     health: 100,
     lv: 0,
     status: 'ready to kick ass',
-    img: 'https://slack-imgs.com/?c=1&o1=ro&url=https%3A%2F%2Fth.bing.com%2Fth%2Fid%2FR.6990db9caf2e499b4b69a4b28e29637b%3Frik%3Dc9mm8cevOSRxtw%26riu%3Dhttp%253a%252f%252fimg07.deviantart.net%252f72af%252fi%252f2011%252f345%252ff%252f0%252fi_geek_weekly__gandalf_the_grey_by_joshuafitzpatrick-d4iufqz.png%26ehk%3D5C1BwJIW1HQSLERf7KDG%252fNepgVD3K%252bFx1k2h4AdOst4%253d%26risl%3D%26pid%3DImgRaw%26r%3D0)',
+    img: 'https://th.bing.com/th/id/R.6990db9caf2e499b4b69a4b28e29637b?rik=c9mm8cevOSRxtw&riu=http%3a%2f%2fimg07.deviantart.net%2f72af%2fi%2f2011%2f345%2ff%2f0%2fi_geek_weekly__gandalf_the_grey_by_joshuafitzpatrick-d4iufqz.png&ehk=5C1BwJIW1HQSLERf7KDG%2fNepgVD3K%2bFx1k2h4AdOst4%3d&risl=&pid=ImgRaw&r=0',
   },
 }
 const boss = {
@@ -11,7 +11,7 @@ const boss = {
     health: 100,
     lv: 0,
     status: 'evil mofo',
-    img: 'https://slack-imgs.com/?c=1&o1=ro&url=https%3A%2F%2Fdesignlooter.com%2Fimages%2Fbalrog-coloring-18.jpg',
+    img: 'https://designlooter.com/images/balrog-coloring-18.jpg',
     },
 }
 
@@ -21,13 +21,13 @@ function drawHero() {
   let template = ""
   for (let key in hero) {
     let chars = hero[key]
-    console.log(hero, "charater")
+    console.log(hero.health)
     template += ` 
     <div class="col-3 hero-thing">
-    <div class="m-0">NAME : ${key}</div>
-    <img class="align-items-left "src="${hero.img}" class="m-1">
-    <p><b>HP : ${hero.health}</b></p>
-    <p><b>Lvl : ${hero.lv}</b></p>
+    <div class="m-0"><b>NAME : ${key}</b></div>
+    <img class="align-items-left "src="${chars.img}" class="m-1">
+    <p><b>HP : ${chars.health}</b></p>
+    <p><b>Lvl : ${chars.lv}</b></p>
 </div>`
     let charElm = document.getElementById('char')
     charElm.innerHTML = template
@@ -43,15 +43,15 @@ function drawBoss() {
     console.log(boss, "charater")
     template += `         
     <div class="col-10 offset-1 boss-card text-center" >
-    <div class="m-0">NAME : ${key}</div>
+    <div class="m-0"><b>NAME : ${key}</b></div>
     <img class="align-items-left m-1"
-      src="${boss.img}">
+      src="${chars.img}">
     <div class="progress">
       <div class="progress-bar progress-bar-striped progress-bar-animated bg-danger" role="progressbar"
         aria-label="Animated striped example" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"
-        style="width: ${boss.health}%"></div>
+        style="width: ${chars.health}%"></div>
     </div>
-    <p><b>Lvl : 4</b></p>
+    <p><b>Lvl : ${chars.lv}</b></p>
   </div>
     `
     let charElm = document.getElementById('boss')
