6276cf8cf413bc312b9fd70c91ffa92157cda445
Garrett Adams <garrettadams27@gmail.com>
2022-07-21 16:07:31 -0600

    d

--- app.js
+++ app.js
@@ -2,15 +2,17 @@ const hero = {
   Gandalf: {
     health: 100,
     lv: 0,
-    status: 'ready to kick ass'
+    status: 'ready to kick ass',
+    img: 'https://slack-imgs.com/?c=1&o1=ro&url=https%3A%2F%2Fth.bing.com%2Fth%2Fid%2FR.6990db9caf2e499b4b69a4b28e29637b%3Frik%3Dc9mm8cevOSRxtw%26riu%3Dhttp%253a%252f%252fimg07.deviantart.net%252f72af%252fi%252f2011%252f345%252ff%252f0%252fi_geek_weekly__gandalf_the_grey_by_joshuafitzpatrick-d4iufqz.png%26ehk%3D5C1BwJIW1HQSLERf7KDG%252fNepgVD3K%252bFx1k2h4AdOst4%253d%26risl%3D%26pid%3DImgRaw%26r%3D0)',
   },
 }
 const boss = {
   Balrog: {
     health: 100,
     lv: 0,
-    status: 'evil mofo'
-  },
+    status: 'evil mofo',
+    img: 'https://slack-imgs.com/?c=1&o1=ro&url=https%3A%2F%2Fdesignlooter.com%2Fimages%2Fbalrog-coloring-18.jpg',
+    },
 }
 
 
@@ -22,10 +24,10 @@ function drawHero() {
     console.log(hero, "charater")
     template += ` 
     <div class="col-3 hero-thing">
-    <div class="m-0">NAME : FRANK</div>
-    <img class="align-items-left "src="https://slack-imgs.com/?c=1&o1=ro&url=https%3A%2F%2Fth.bing.com%2Fth%2Fid%2FR.6990db9caf2e499b4b69a4b28e29637b%3Frik%3Dc9mm8cevOSRxtw%26riu%3Dhttp%253a%252f%252fimg07.deviantart.net%252f72af%252fi%252f2011%252f345%252ff%252f0%252fi_geek_weekly__gandalf_the_grey_by_joshuafitzpatrick-d4iufqz.png%26ehk%3D5C1BwJIW1HQSLERf7KDG%252fNepgVD3K%252bFx1k2h4AdOst4%253d%26risl%3D%26pid%3DImgRaw%26r%3D0" class="m-1">
-    <p><b>HP : X</b></p>
-    <p><b>Lvl : 4</b></p>
+    <div class="m-0">NAME : ${key}</div>
+    <img class="align-items-left "src="${hero.img}" class="m-1">
+    <p><b>HP : ${hero.health}</b></p>
+    <p><b>Lvl : ${hero.lv}</b></p>
 </div>`
     let charElm = document.getElementById('char')
     charElm.innerHTML = template
@@ -41,14 +43,13 @@ function drawBoss() {
     console.log(boss, "charater")
     template += `         
     <div class="col-10 offset-1 boss-card text-center" >
-    <div class="m-0">NAME : FRANK</div>
+    <div class="m-0">NAME : ${key}</div>
     <img class="align-items-left m-1"
-      src="https://slack-imgs.com/?c=1&o1=ro&url=https%3A%2F%2Fth.bing.com%2Fth%2Fid%2FR.6990db9caf2e499b4b69a4b28e29637b%3Frik%3Dc9mm8cevOSRxtw%26riu%3Dhttp%253a%252f%252fimg07.deviantart.net%252f72af%252fi%252f2011%252f345%252ff%252f0%252fi_geek_weekly__gandalf_the_grey_by_joshuafitzpatrick-d4iufqz.png%26ehk%3D5C1BwJIW1HQSLERf7KDG%252fNepgVD3K%252bFx1k2h4AdOst4%253d%26risl%3D%26pid%3DImgRaw%26r%3D0"
-      class="m-1">
+      src="${boss.img}">
     <div class="progress">
       <div class="progress-bar progress-bar-striped progress-bar-animated bg-danger" role="progressbar"
         aria-label="Animated striped example" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"
-        style="width: 100%"></div>
+        style="width: ${boss.health}%"></div>
     </div>
     <p><b>Lvl : 4</b></p>
   </div>
